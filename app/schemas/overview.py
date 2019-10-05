6ef04a2817ef7cbd91fd81953420815001534980
Slyfoxy <khitrov34@gmail.com>
2019-10-05 16:55:52 +0300

    change 'ComputerOverview' to 'ComputerDetails' and add 'ComputerInList'

--- app/schemas/overview.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class ComputerOverview(BaseModel):
-    pass  # todo add overview fields for pc
--- app/schemas/registration.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from app.schemas.overview import ComputerOverview
-
-
-class ComputerRegistration(BaseModel):
-    mac_address: str
-    name: str
-    overview: ComputerOverview
