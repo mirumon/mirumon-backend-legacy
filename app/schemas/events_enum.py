83ab071ed81e51e6d9eda28456c9fa5825c138ec
Slyfoxy <khitrov34@gmail.com>
2019-10-05 16:57:08 +0300

    rename 'overview' event to 'details', remove 'os', add 'auth'

--- app/schemas/events_enum.py
+++ app/schemas/events_enum.py
@@ -3,14 +3,19 @@ from enum import Enum
 
 class EventTypeEnum(Enum):
     registration = "registration"
-    overview = "overview"
-    os = "os"
+    auth: str = "auth"
+
+    details = "details"
+
+    users = "users"
+
     system = "system"
     hardware = "hardware"
     storage = "storage"
     network = "network"
+
     devices = "devices"
-    users = "users"
+
     installed_programs = "installed-programs"
     startup_programs = "startup-programs"
     services = "services"
--- app/schemas/status_enum.py
+++ app/schemas/status_enum.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class StatusEnum(Enum):
     registration_success: str = "registration_success"
-    auth_success: str = "auth_success"
     registration_failed: str = "registration_failed"
+    auth_success: str = "auth_success"
     auth_failed: str = "auth_failed"
