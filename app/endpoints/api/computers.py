d7771c3f5824c6f42113b179e3938b650b345a8a
igorpackin <altair19012000@gmail.com>
2019-11-03 04:59:42 +0300

    add generic view generator

--- app/endpoints/api/computers.py
+++ app/endpoints/api/computers.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Tuple, Type, TypeVar
 
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from starlette import status
 from starlette.websockets import WebSocketDisconnect
 
-from app.schemas.computers.details import ComputerInList
+from app.schemas.computers.details import ComputerDetails, ComputerInList
 from app.schemas.events.rest import EventInRequest, EventPayload, RestEventType
 from app.services.computers import ClientsManager, get_clients_manager
 from app.services.event_handlers import clients_list
@@ -54,3 +55,44 @@ async def computer_events(
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=error_detail
         )
+
+
+####
+
+T = TypeVar("T", bound=BaseModel)
+
+
+def fill_app(values: Tuple[Tuple[str, Type[T]], ...]) -> None:
+    for event_type, model in values:
+        path = "computers/{mac_address}/" + event_type
+
+        @router.get(path, response_model=model)
+        async def func(
+            mac_address: str,
+            clients_manager: ClientsManager = Depends(get_clients_manager),
+            events_manager: EventsManager = Depends(get_events_manager),
+        ) -> T:
+            try:
+                client = clients_manager.get_client(mac_address)
+            except KeyError as missed_websocket_error:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="PC not found"
+                ) from missed_websocket_error
+
+            event = events_manager.generate_event(event_type)
+            await client.send_event(EventInRequest(event=event))
+            try:
+                return await events_manager.wait_event_from_client(
+                    event_id=event.id, client=client
+                )
+            except WebSocketDisconnect:
+                if client.is_connected:
+                    error_detail = f"{event_type} event is not supported by PC"
+                else:
+                    error_detail = "PC disconnected"
+                raise HTTPException(
+                    status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=error_detail
+                )
+
+
+fill_app((("details", ComputerDetails),))
