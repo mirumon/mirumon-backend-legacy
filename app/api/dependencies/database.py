b0a43a4dca323fbf30704d8b5ed09ce90d5a7e1f
Nik Sidnev <sidnev.nick@gmail.com>
2019-12-01 15:11:27 +0300

    add repos dependency

---
+++ app/api/dependencies/database.py
@@ -0,0 +1,21 @@
+from typing import AsyncGenerator, Callable, Type
+
+from asyncpg.pool import Pool
+from fastapi import Depends
+from starlette.requests import Request
+
+from app.db.repositories.base import BaseRepository
+
+
+def _get_db_pool(request: Request) -> Pool:
+    return request.app.state.pool
+
+
+def get_repository(repo_type: Type[BaseRepository]) -> Callable:  # type: ignore
+    async def _get_repo(
+            pool: Pool = Depends(_get_db_pool),
+    ) -> AsyncGenerator[BaseRepository, None]:
+        async with pool.acquire() as conn:
+            yield repo_type(conn)
+
+    return _get_repo
