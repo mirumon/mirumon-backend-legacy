eaeeeb6d23108e7d4f288f0f2e90841397ad96c5
Slyfoxy <khitrov34@gmail.com>
2020-01-28 00:17:17 +0300

    Add type annotations for some vars in config

--- app/common/config.py
+++ app/common/config.py
@@ -16,7 +16,7 @@ JWT_TOKEN_TYPE: str = "Bearer"
 
 config = Config(".env")
 
-REST_MAX_RESPONSE_TIME = config(
+REST_MAX_RESPONSE_TIME: float = config(
     "REST_MAX_RESPONSE_TIME", cast=float, default=5.0  # noqa: WPS432
 )
 REST_SLEEP_TIME: float = config(
@@ -26,7 +26,7 @@ REST_SLEEP_TIME: float = config(
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
 DATABASE_URL: DatabaseURL = config("DB_CONNECTION", cast=DatabaseURL)
 SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
-SHARED_TOKEN = config("SHARED_TOKEN", cast=str)
+SHARED_TOKEN: str = config("SHARED_TOKEN", cast=str)
 
 LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
 
