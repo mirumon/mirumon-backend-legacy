e907c493307f8a3ef2a0b4d1ff158f37747697fd
Nick Khitrov <khitrov34@gmail.com>
2020-10-22 01:49:50 +0300

    Split create device method to method with shared key and scopes (#22)

--- app/api/endpoints/http/devices/auth.py
+++ app/api/endpoints/http/devices/auth.py
@@ -3,7 +3,9 @@ from starlette import status
 from starlette.status import HTTP_400_BAD_REQUEST
 
 from app.api.dependencies.services import get_service
+from app.api.dependencies.users.permissions import check_user_scopes
 from app.api.models.http.devices.auth import DeviceAuthInRequest, DeviceAuthInResponse
+from app.domain.user.scopes import DevicesScopes
 from app.resources import strings
 from app.services.devices.auth_service import DevicesAuthService
 from app.services.devices.devices_service import DevicesService
@@ -18,8 +20,25 @@ router = APIRouter()
     summary="Create device",
     description=strings.DEVICES_CREATE_DESCRIPTION,
     response_model=DeviceAuthInResponse,
+    dependencies=[Depends(check_user_scopes([DevicesScopes.write]))],
 )
 async def create_device(
+    auth_service: DevicesAuthService = Depends(get_service(DevicesAuthService)),
+    devices_service: DevicesService = Depends(get_service(DevicesService)),
+) -> DeviceAuthInResponse:
+    device = await devices_service.register_new_device()
+    token = auth_service.create_device_token(device)
+    return DeviceAuthInResponse(token=token)
+
+
+@router.post(
+    "/by/shared",
+    status_code=status.HTTP_201_CREATED,
+    name="devices:create-by-shared",
+    summary="Create device by Shared Key",
+    response_model=DeviceAuthInResponse,
+)
+async def create_device_by_shared_key(
     credentials: DeviceAuthInRequest,
     auth_service: DevicesAuthService = Depends(get_service(DevicesAuthService)),
     devices_service: DevicesService = Depends(get_service(DevicesService)),
