01cc764ce39ddcab9b4f440c0bb71d46a4700424
Slyfoxy <khitrov34@gmail.com>
2020-10-29 02:44:52 +0300

    Remove Entity from domain package and update Device model

--- app/domain/devices/device.py
+++ app/domain/devices/device.py
@@ -1,10 +1,17 @@
+import uuid
+from dataclasses import dataclass
 from typing import NewType
 from uuid import UUID
 
-from app.domain.base import Entity
+from app.domain.core.model import DomainModel
 
 DeviceID = NewType("DeviceID", UUID)
 
 
-class Device(Entity):
+@dataclass
+class Device(DomainModel):
     id: DeviceID
+
+    @classmethod
+    def generate_id(cls) -> DeviceID:
+        return DeviceID(uuid.uuid4())
