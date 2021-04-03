2ff27b15d2db59f15e80128cfd2f182de45b2c8c
Nick Khitrov <khitrov34@gmail.com>
2021-04-03 13:31:02 +0300

    Update fake client

--- tests/integration/support/fake_device.py
+++ tests/integration/support/fake_device.py
@@ -87,53 +87,53 @@ class FakeDevice:
         }
         events["sync_device_hardware"] = {
             "motherboard": {
-                "name": "string",
-                "caption": "string",
-                "status": "string",
-                "product": "string",
-                "serial_number": "string",
+                "status": "ok",
+                "name": "MSI b450",
+                "caption": "MSI b450 Tomahawk",
+                "product": "MSI",
+                "serial_number": "384134g141ghwg92",
             },
             "cpu": [
                 {
-                    "status": "string",
-                    "name": "string",
-                    "caption": "string",
-                    "current_clock_speed": "string",
+                    "status": "ok",
+                    "name": "AMD Ryzen 5",
+                    "caption": "AMD Ryzen 5",
+                    "current_clock_speed": "100",
                     "current_cthread_countlock_speed": 0,
                     "virtualization_firmware_enabled": True,
-                    "load_percentage": 0,
-                    "number_of_cores": 0,
-                    "number_of_enabled_core": 0,
-                    "number_of_logical_processors": 0,
+                    "load_percentage": 50,
+                    "number_of_cores": 12,
+                    "number_of_enabled_core": 6,
+                    "number_of_logical_processors": 6,
                 }
             ],
             "gpu": [
                 {
-                    "status": "string",
-                    "name": "string",
-                    "caption": "string",
-                    "driver_version": "string",
-                    "driver_date": "string",
-                    "video_mode_description": "string",
-                    "current_vertical_resolution": "string",
+                    "status": "ok",
+                    "name": "gtx 970",
+                    "caption": "Nvidea GTX 970",
+                    "driver_version": "370.9",
+                    "driver_date": "12.12.12",
+                    "video_mode_description": "no description",
+                    "current_vertical_resolution": "1024x1024",
                 }
             ],
             "network": [
                 {
-                    "description": "string",
-                    "mac_address": "string",
-                    "ip_addresses": ["string"],
+                    "description": "eth0",
+                    "mac_address": "00:1B:44:11:3A:B7",
+                    "ip_addresses": ["192.158.1.37", "192.158.1.38"],
                 }
             ],
             "disks": [
                 {
-                    "status": "string",
-                    "caption": "string",
-                    "serial_number": "string",
-                    "size": 0,
-                    "model": "string",
-                    "description": "string",
-                    "partitions": 0,
+                    "status": "ok",
+                    "caption": "Disk 1",
+                    "serial_number": "123123213123",
+                    "size": 10000,
+                    "model": "samsung",
+                    "description": "HDD",
+                    "partitions": 2,
                 }
             ],
         }
